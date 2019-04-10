b0c843b61b21d3b2a49a817d3a16d84d024bd393
Piotr Gajowniczek <piotrgajow@gmail.com>
2019-04-10 21:01:08 +0200

    Add bootstrap 4 styled version

--- main.js
+++ main.js
@@ -1,37 +1,72 @@
 let quizId;
 
-const API = 'http://localhost:8080';
+const API = 'http://localhost:8090';
+
+const CLASS_INVISIBLE = 'invisible';
+const CLASS_CORRECT = 'bg-success';
+const CLASS_WRONG = 'bg-danger';
+
 const NAME_ELEMENT = document.getElementById('name');
 const RESULT_ELEMENT = document.getElementById('result');
 const LINK_ELEMENT = document.getElementById('link');
 const DESCRIPTION_ELEMENT = document.getElementById('description');
 const SCORE_ELEMENT = document.getElementById('score');
+const QUESTION_ELEMENT = document.getElementById('question');
+const ANSWER_ELEMENTS = document.querySelectorAll('.btn-answer');
+const NEXT_QUESTION_ELEMENT = document.getElementById('next-question');
+const MODAL_SCORE_ELEMENT = document.getElementById('modal-score');
+
+let score = 0;
+
+const STATE_NO_QUIZ = 0;
+const STATE_QUESTION = 1;
+const STATE_RESULT = 2;
+let state = STATE_NO_QUIZ;
 
 function url(endpoint) {
     return `${API}${endpoint}`;
 }
 
+function nextQuestion() {
+    if (state === STATE_NO_QUIZ) {
+        startNewQuiz();
+    } else {
+        loadQuestion();
+    }
+}
+
 async function startNewQuiz() {
+    if (state !== STATE_NO_QUIZ) {
+        return;
+    }
     const response = await fetch(url('/quiz'), { method: 'POST' });
     const result = await response.json();
     quizId = result.id;
+    onQuizStarted();
     await loadQuestion();
 }
 
 async function loadQuestion() {
+    if (state === STATE_QUESTION ) {
+        return;
+    }
     const response = await fetch(url(`/quiz/${quizId}/question`));
     const result = await response.json();
     if (response.status === 200) {
-        NAME_ELEMENT.innerText = result.name;
-        RESULT_ELEMENT.innerText = '';
-        LINK_ELEMENT.innerText = '';
-        DESCRIPTION_ELEMENT.innerText = '';
+        if (result.done) {
+            onQuizEnd();
+        } else {
+            onNextQuestion(result.name);
+        }
     } else {
         alert(result.error);
     }
 }
 
 async function answer(choice) {
+    if (state !== STATE_QUESTION) {
+        return;
+    }
     const request = { answer: choice };
     const options = {
         method: 'POST',
@@ -43,17 +78,47 @@ async function answer(choice) {
     const response = await fetch(url(`/quiz/${quizId}/answer`), options);
     const result = await response.json();
     if (response.status === 200) {
-        showResult(result);
+        onAnswerResult(result);
     } else {
         alert(result.error);
     }
     await getScore();
 }
 
-function showResult(result) {
+async function getScore() {
+    const response = await fetch(url(`/quiz/${quizId}/score`));
+    const result = await response.json();
+    if (response.status === 200) {
+        updateScore(result.score);
+    } else {
+        alert(result.error);
+    }
+}
+
+function onQuizStarted() {
+    updateScore(0);
+    QUESTION_ELEMENT.classList.remove(CLASS_INVISIBLE);
+}
+
+function onNextQuestion(name) {
+    state = STATE_QUESTION;
+    NAME_ELEMENT.innerText = name;
+    RESULT_ELEMENT.innerText = '';
+    LINK_ELEMENT.innerText = '';
+    DESCRIPTION_ELEMENT.innerText = '';
+    ANSWER_ELEMENTS.forEach((it) => it.classList.remove(CLASS_INVISIBLE));
+    QUESTION_ELEMENT.classList.remove(CLASS_CORRECT, CLASS_WRONG);
+    NEXT_QUESTION_ELEMENT.classList.add(CLASS_INVISIBLE);
+}
+
+function onAnswerResult(result) {
+    state = STATE_RESULT;
+    ANSWER_ELEMENTS.forEach((btn) => btn.classList.add(CLASS_INVISIBLE));
     if (result.correct) {
+        QUESTION_ELEMENT.classList.add(CLASS_CORRECT);
         RESULT_ELEMENT.innerText = 'You are correct!';
     } else {
+        QUESTION_ELEMENT.classList.add(CLASS_WRONG);
         RESULT_ELEMENT.innerText = 'You are wrong!';
     }
 
@@ -65,14 +130,41 @@ function showResult(result) {
     } else {
         DESCRIPTION_ELEMENT.innerText = ' is IT related name!';
     }
+
+    NEXT_QUESTION_ELEMENT.classList.remove(CLASS_INVISIBLE);
 }
 
-async function getScore() {
-    const response = await fetch(url(`/quiz/${quizId}/score`));
-    const result = await response.json();
-    if (response.status === 200) {
-        SCORE_ELEMENT.innerText = result.score;
+function onQuizEnd() {
+    state = STATE_NO_QUIZ;
+    QUESTION_ELEMENT.classList.add(CLASS_INVISIBLE);
+    MODAL_SCORE_ELEMENT.innerText = score;
+    if (score >= 7) {
+        MODAL_SCORE_ELEMENT.classList = 'text-success';
+    } else if (score <= 3) {
+        MODAL_SCORE_ELEMENT.classList = 'text-danger';
     } else {
-        alert(result.error);
+        MODAL_SCORE_ELEMENT.classList ='text-warning';
     }
+
+    $('#quizEndedModal').modal({});
 }
+
+function updateScore(newScore) {
+    score = newScore;
+    SCORE_ELEMENT.innerText = score;
+}
+
+window.addEventListener('keyup', (event) => {
+    console.log(event);
+    switch (event.key) {
+        case "ArrowLeft":
+            answer(0);
+            break;
+        case "ArrowRight":
+            answer(1);
+            break;
+        case " ":
+            nextQuestion();
+            break;
+    }
+});
