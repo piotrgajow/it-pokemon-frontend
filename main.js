58c44eeeb3326c1c869fc81e937dc92283dc8085
Piotr Gajowniczek <piotrgajow@gmail.com>
2019-06-20 12:46:52 +0200

    Switch to flow from grid

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ let quizId;
 
 const API = 'http://localhost:8090';
 
-const CLASS_INVISIBLE = 'invisible';
+const CLASS_INVISIBLE = 'hidden';
 const CLASS_CORRECT = 'bg-success';
 const CLASS_WRONG = 'bg-danger';
 
+const MAIN_ELEMENT = document.getElementsByTagName('main')[0];
 const NAME_ELEMENT = document.getElementById('name');
 const RESULT_ELEMENT = document.getElementById('result');
 const LINK_ELEMENT = document.getElementById('link');
 const DESCRIPTION_ELEMENT = document.getElementById('description');
 const SCORE_ELEMENT = document.getElementById('score');
-const QUESTION_ELEMENT = document.getElementById('question');
 const ANSWER_ELEMENTS = [document.getElementById('btn-answer-0'), document.getElementById('btn-answer-1')];
 const NEXT_QUESTION_ELEMENT = document.getElementById('next-question');
 const MODAL_SCORE_ELEMENT = document.getElementById('modal-score');
@@ -107,7 +107,7 @@ async function getScore() {
 
 function onQuizStarted() {
     updateScore(0);
-    QUESTION_ELEMENT.classList.remove(CLASS_INVISIBLE);
+    MAIN_ELEMENT.classList.remove(CLASS_INVISIBLE);
 }
 
 function onNextQuestion(name) {
@@ -117,7 +117,7 @@ function onNextQuestion(name) {
     LINK_ELEMENT.innerText = '';
     DESCRIPTION_ELEMENT.innerText = '';
     ANSWER_ELEMENTS.forEach((it) => it.classList.remove(CLASS_INVISIBLE));
-    QUESTION_ELEMENT.classList.remove(CLASS_CORRECT, CLASS_WRONG);
+    MAIN_ELEMENT.classList.remove(CLASS_CORRECT, CLASS_WRONG);
     NEXT_QUESTION_ELEMENT.classList.add(CLASS_INVISIBLE);
 }
 
@@ -125,10 +125,10 @@ function onAnswerResult(result) {
     state = STATE_RESULT;
     ANSWER_ELEMENTS.forEach((btn) => btn.classList.add(CLASS_INVISIBLE));
     if (result.correct) {
-        QUESTION_ELEMENT.classList.add(CLASS_CORRECT);
+        MAIN_ELEMENT.classList.add(CLASS_CORRECT);
         RESULT_ELEMENT.innerText = 'You are correct!';
     } else {
-        QUESTION_ELEMENT.classList.add(CLASS_WRONG);
+        MAIN_ELEMENT.classList.add(CLASS_WRONG);
         RESULT_ELEMENT.innerText = 'You are wrong!';
     }
 
@@ -146,7 +146,7 @@ function onAnswerResult(result) {
 
 function onQuizEnd() {
     state = STATE_NO_QUIZ;
-    QUESTION_ELEMENT.classList.add(CLASS_INVISIBLE);
+    MAIN_ELEMENT.classList.add(CLASS_INVISIBLE);
     MODAL_SCORE_ELEMENT.innerText = score;
     if (score >= 7) {
         MODAL_SCORE_ELEMENT.classList = 'text-success';
